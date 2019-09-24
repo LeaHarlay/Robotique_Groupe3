6e07350e4a932f7ea50c45084027325908733fc0
amelie21 <amelied28@live.fr>
2019-09-24 10:00:18 +0200

    Packages et Classes

---
+++ src/comportements/Avancer.java
@@ -0,0 +1,5 @@
+package comportements;
+
+public class Avancer {
+
+}
--- src/robots/GardeNuit.java
+++ src/robots/GardeNuit.java
@@ -1,6 +1,6 @@
-package test;
+package robots;
 
-public class Main {
+public class GardeNuit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
