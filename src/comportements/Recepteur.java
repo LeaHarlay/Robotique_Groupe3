ffb62af715444fc75d6e3bd07a67c0faced30463
amelie21 <amelied28@live.fr>
2019-11-13 14:08:27 +0100

    Suppression d'essais

--- src/comportements/Recepteur.java
+++ src/comportements/Recepteur.java
@@ -11,164 +11,75 @@ import lejos.robotics.subsumption.Behavior;
 import lejos.utility.Delay;
 
 public class Recepteur implements Behavior {
-	private String bluetoothRobot1="00:16:53:43:EB:88";
-	private String bluetoothRobot2="00:16:53:43:B3:FB";
 	private NXTConnection btc;
 	private BTConnector bt;
 	InputStream reponse;
 	ObjectInputStream oReponse;
-	Object valeurO ;
+	Object valeurO;
 
 	public boolean takeControl() {
-		if(Button.ENTER.isDown()) {
-			return true;
-			/*
-			if (this.bt == null) {
-				this.bt = new BTConnector();
-			}
-			try {
-				System.out.println("methode1");
-				this.btc = this.bt.waitForConnection(1000, NXTConnection.PACKET);
-				//this.btc = this.bt.connect(bluetoothRobot1, NXTConnection.PACKET);
-				System.out.println("btc :"+this.btc);
-				this.reponse = this.btc.openInputStream();
-				System.out.println("reponse : "+this.reponse);
-				this.oReponse = new ObjectInputStream(reponse);
-				this.valeurO = this.oReponse.readObject();
-				this.oReponse.close();
-				this.btc.close();
-				this.bt.cancel();
-				return (this.valeurO != null);
-			}catch(Exception e) {
-				return false;
-			}
-			*/
-			/*
-			try{
-				
-				System.out.println("methode2");
-				this.btc = this.bt.connect(bluetoothRobot2, NXTConnection.PACKET);
-				System.out.println("btc :"+this.btc);
-				this.reponse = this.btc.openInputStream();
-				System.out.println("reponse : "+this.reponse);
-				this.oReponse = new ObjectInputStream(reponse);
-				this.valeurO = this.oReponse.readObject();
-				this.oReponse.close();
-				this.btc.close();
-				this.bt.cancel();
-			}catch(Exception e) {
-			}
-			Delay.msDelay(1000);
-			return (this.valeurO != null);
-			*/
-		}else {
-			return false;
-		}
-			
-			//return this.btc != null;
+		return Button.ENTER.isDown();
 	}
+
 	public void suppress() {
 	}
 
 	public void action() {
-		
+
 		if (this.bt == null) {
 			this.bt = new BTConnector();
 		}
-		/*try {
-			System.out.println("methode1");
-			this.btc = this.bt.waitForConnection(1000, NXTConnection.PACKET);
-			//this.btc = this.bt.connect(bluetoothRobot1, NXTConnection.PACKET);
-			this.reponse = this.btc.openInputStream();
-			this.oReponse = new ObjectInputStream(reponse);
-			this.valeurO = this.oReponse.readObject();
-			this.oReponse.close();
-			this.btc.close();
-			this.bt.cancel();
-			//return (this.valeurO != null);
-		}catch(Exception e) {
-			//return false;
-		}*/
-		
+
 		LCD.drawString("Connection", 0, 0);
 		LCD.clear();
 		LCD.refresh();
 		try {
 			System.out.println("methode1");
 			this.btc = this.bt.waitForConnection(1000, NXTConnection.PACKET);
-			//this.btc = this.bt.connect(bluetoothRobot1, NXTConnection.PACKET);
 			this.reponse = this.btc.openInputStream();
 			this.oReponse = new ObjectInputStream(reponse);
 			this.valeurO = this.oReponse.readObject();
 			this.oReponse.close();
 			this.btc.close();
 			this.bt.cancel();
-			//this.btc = bt.waitForConnection(1000, NXTConnection.PACKET);
-			//InputStream reponse = this.btc.openInputStream();
-			
-			//DataInputStream dReponse = new DataInputStream(reponse);
-			//ObjectInputStream oReponse = new ObjectInputStream(reponse);
-			
-			
-			//int valeur = dReponse.read();
-			//Object valeurO = oReponse.readObject();
 
-			// Arrêt
-			//dReponse.close();
-			/*
-			oReponse.close();
-			this.btc.close();
-			this.bt.cancel();*/
-			
-			//Affichage de la réponse
+			// Affichage de la réponse
 			LCD.clear();
 			LCD.refresh();
-			//System.out.println(valeur);
-			
-			//System.out.println("Je vais afficher...");
+
 			affichageObjetRecu(this.valeurO);
-			//System.out.println(valeurO);
-			
-			/*if (valeurO instanceof Case) {
-				valeurO = (Case) valeurO;
-				System.out.println(((Case) valeurO).getNom());
-				
-			}else {
-				System.out.println("Ca n'a pas march�");
-			}*/
-			
 			Delay.msDelay(5000);
-			
+
 			System.out.println("\n\n\n\n\n\n\n");
-			
+
 			LCD.clear();
 			LCD.refresh();
 		} catch (Exception e) {
 			System.out.println("catch recepteur");
 		}
 	}
-	
-	public static void affichageObjetRecu(Object valeur){
+
+	public static void affichageObjetRecu(Object valeur) {
 		if (valeur instanceof int[][]) {
 			int[][] val = (int[][]) valeur;
-			for (int i = 0 ; i<7 ; i++) {
+			for (int i = 0; i < 7; i++) {
 				String[] tab = new String[5];
-				for (int j = 0; j<5;j++) {
-					if(val[i][j]==0) {
-						tab[j]=" x "; //connu
-					}else if(val[i][j]==1) {
-						tab[j]=" _ "; //inconnu
-					}else {
-						tab[j]=" o "; //position
-					}	
+				for (int j = 0; j < 5; j++) {
+					if (val[i][j] == 0) {
+						tab[j] = " x "; // connu
+					} else if (val[i][j] == 1) {
+						tab[j] = " _ "; // inconnu
+					} else {
+						tab[j] = " o "; // position
+					}
 				}
-				System.out.println(tab[0]+tab[1]+tab[2]+tab[3]+tab[4]);
-				//LCD.drawString(tab[0]+tab[1]+tab[2]+tab[3]+tab[4],0,i);
+				System.out.println(tab[0] + tab[1] + tab[2] + tab[3] + tab[4]);
+				// LCD.drawString(tab[0]+tab[1]+tab[2]+tab[3]+tab[4],0,i);
 			}
-		}else {
-			//LCD.drawString("Pas m�me objet",0,2);
+		} else {
+			// LCD.drawString("Pas m�me objet",0,2);
 			System.out.println("Pas m�me objet");
-			
+
 		}
 
 	}
